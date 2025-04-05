fe21250f3dd892568c69fb83a1f57c1b304d4ed6
masonnguyen1243 <meowjcisgod@gmail.com>
2025-04-05 15:33:44 +0700

    first commit

---
+++ eslint.config.js
@@ -0,0 +1,39 @@
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+
+export default tseslint.config(
+  { ignores: ["dist", "ignores"] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: eslintPluginPrettier,
+    },
+    rules: {
+      "prettier/prettier": [
+        "warn",
+        {
+          arrowParens: "always",
+          tabWidth: 2,
+          endOfLine: "auto",
+          useTabs: false,
+          printWidth: 120,
+          jsxSingleQuote: true,
+        },
+      ],
+      "no-console": "error",
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+    },
+  },
+);
